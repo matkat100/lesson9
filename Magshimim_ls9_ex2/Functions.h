43434a05eb94e6c2edbb675d5ab4f7ba0bf43325
matkat100 <matiskino@gmail.com>
2025-01-13 19:56:51 +0200

    added compare

---
+++ Magshimim_ls9_ex2/Functions.h
@@ -0,0 +1,35 @@
+﻿#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+using namespace std;
+
+template <typename T> int compare(const T& a, const T& b)
+{
+    if (a > b)
+    {
+        return 1;
+    }
+    else if (a < b) 
+    {
+        return -1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+template <typename T> int bubbleSort(T& arr, const T& size)
+{
+    for (int i = 0; i < size; i++) 
+    {
+        for (int j = 0; j < size; j++) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+#endif 
